e531d376f83571f4a1ff3d0966e757f00bc67cd9
Bruce Harris <bharris845@gmail.com>
2012-05-11 14:45:16 -0400

    refactoring for cancel/fail handling

--- slick.lazyloader.js
+++ slick.lazyloader.js
@@ -1,51 +1,91 @@
 (function ($) {
     /**
      * Loads data lazily (as needed) from a remote data source
-     * @param {Function}       fetchData     Fetches data from remote source and loads it into local cache. This is a method
-     *                                       that will be added to the protoype of the Range constructor defined below. An 
-     *                                       instance of Range represents a range of rows, has properties .top and .bottom
-     *                                       which are to be used in the fetchData method to tell the data source what rows
-     *                                       to return. After data is retrieved (presumably in the success handler of the AJAX
-     *                                       call) the range's loadData method must be called. See loadData below.
-     * @param {Array,String}   [fields]      Optional array of field names, only necessary if mapping data cells to fields by column index
-     * @param {Number}         [numRows]     Optional total rows in data source - alternatively can be set by setLength method
-     *                                       if not known at time of instantiation
-     * @param {Number}         [blockSize]   Optional # of rows to retrieve in each fetch (default is 100)
+     * @param {Object}       args
+     * @param {Function}     args.fetchData     Fetches data from remote source and loads it into local cache. This is a method
+     *                                          that will be added to the protoype of the Range constructor defined below. An 
+     *                                          instance of Range represents a range of rows, has properties .top and .bottom
+     *                                          which are to be used in the fetchData method to tell the data source what rows
+     *                                          to return. After data is retrieved (presumably in the success handler of the AJAX
+     *                                          call) the range's loadData method must be called. See loadData below.
+BH     * @param {Number}       [args.timeout]     Optional # of seconds to wait before giving up on an XHR to return
+BH     * @param {Number}       [args.retries]     Optional # of times to retry a failed XHR
+BH     * @param {Number}       [args.queueSize]   Optional max # of outstanding XHRs (fetchData requests)
+     * @param {Array,String} [args.fields]      Optional array of field names, only necessary if mapping data cells to fields by column index
+     * @param {Number}       [args.numRows]     Optional total rows in data source - alternatively can be set by setLength method
+     *                                          if not known at time of instantiation
+     * @param {Number}       [args.blockSize]   Optional # of rows to retrieve in each fetch (default is 100)
      */
     function LazyLoader(args){
         // events
         var onDataLoaded = new Slick.Event();
 
         // data status codes:
-        var VIRGIN = 0, REQUESTED = 1, READY = 2;
+        var VIRGIN = 0, REQUESTED = 1, READY = 2, UNAVAILABLE = 3;
+
+        var timeout = 1000 * (args.timeout || 20), // milliseconds, default 20 seconds
+            retries = args.retries || 0;
 
         // data is fetched one block at a time; data is segmented into blocks of a fixed row size.
         var dataCache = {
             // properties
             length: args.numRows || 1, // default to 1 so things don't break before we get a chance to call setLength
             blockSize: args.blockSize || 100,
-            blocks: [], // this will be an array of arrays
+            blocks: [], // each block of data (rows) is an array; blocks will be an array of arrays
 
             // methods
             getBlockStatus: function (blockIndex) {
                 var b = this.blocks[blockIndex];
-                return (b === undefined ? VIRGIN : (b === null ? REQUESTED : READY) );
+                return (b === undefined ? VIRGIN : ($.isArray(b) ? READY : b) );
             },
+            // stat must be either VIRGIN, REQUESTED, or UNAVAILABLE
             setBlockStatus: function (blockIndex, stat) {
-                if (stat === VIRGIN) delete this.blocks[blockIndex];
-                else if (stat === REQUESTED) this.blocks[blockIndex] = null;
+                delete this.blocks[blockIndex];
+                if (stat !== VIRGIN) this.blocks[blockIndex] = stat;
             }
         };
 
+        // which block is this row in?
         function getBlockIndex(row){
             return Math.floor(row / dataCache.blockSize);
         }
-        // returns the range of rows contained by a given block index
+
+        // what rows are in this block?
+        // i is the index of the block
         function getBlockRange(i){
             var s = dataCache.blockSize;
             return new Range({ top: i * s, bottom: i * s + s - 1 });
         }
-        
+
+        // queue of blocks to fetch
+        var queue = {
+            _q: [], // array of block indices to be fetched
+            _queueSize: args.queueSize || 4,
+            _busy: false,
+
+            doNext: function () {
+                if (!this._q.length) return;
+
+                this._busy = true;
+                getBlockRange(this._q[0]).fetchData();
+                // set timeout and retry as needed
+            },
+            // i is the index of the block to fetch
+            push: function (i) {
+                // if there are queueSize items in the queue
+                    // cancel the last one and make room for this one
+                // if i is not contiguous with recent, cancel others
+                // if not busy, doNext
+                // loadData needs to shift the queue
+            },
+            shift: function () {
+                this._busy = false;
+                this._q.shift();
+                this.doNext();
+            },
+        };
+
+
         
         // constructor
         // bounds is an object with properties top, bottom
@@ -56,8 +96,41 @@
 
         $.extend(Range.prototype, {
             fetchData: args.fetchData,
+            cancelFetch: args.cancelFetch || function () {
+                // tell server we don't need it anymore
+                // tell browser to cancel xhr?
+                // shift queue?
+                this.markVirgin();
+            },
+            failFetch: function () {
+                // BH ???
+                // tell server we don't need it anymore
+                // tell browser to cancel xhr?
+                // shift queue?
+                this.markVirgin();
+            },
+            failFetch: function () {
+                // BH ???
+                this.markUnavailable();
+            },
             // apply a function for each block in this range
-            // fn takes a single argument: the index of the block to operate on
+            // fn is a function that takes a single argument: the index of the block to operate on
+            _forEachBlock: function (fn) {
+                var last = getBlockIndex(this.bottom);
+                for (var i=getBlockIndex(this.top); i<=last; i++) fn(i);
+            },
+            isDataReady: function () {
+                var isReady = true;
+                this._forEachBlock(function (i) {
+                    if (dataCache.getBlockStatus(i) !== READY) isReady = false;
+                });
+                return isReady;
+            },
+            ensureDataLoaded: function () {
+                this.markUnavailable();
+            },
+            // apply a function for each block in this range
+            // fn is a function that takes a single argument: the index of the block to operate on
             _forEachBlock: function (fn) {
                 var last = getBlockIndex(this.bottom);
                 for (var i=getBlockIndex(this.top); i<=last; i++) fn(i);
@@ -84,6 +157,12 @@
             markVirgin: function () {
                 this._forEachBlock(function (i) {
                     dataCache.setBlockStatus(i, VIRGIN);
+                });
+                return this;
+            },
+            markUnavailable: function () {
+                this._forEachBlock(function (i) {
+                    dataCache.setBlockStatus(i, UNAVAILABLE);
                 });
                 return this;
             },
@@ -125,7 +204,7 @@
             },
             getItem: function (i) {
                 var block = dataCache.blocks[getBlockIndex(i)];
-                return block && block[i % dataCache.blockSize];
+                return $.isArray(block) && block[i % dataCache.blockSize];
             },
             getLength: function () {
                 return dataCache.length;
